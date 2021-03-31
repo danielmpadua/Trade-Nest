5e346638d5970cf163548fa6cbfc1ac0aab2251f
LucasGSilva21 <lucasgsilva2102@gmail.com>
2021-03-31 09:31:07 -0300

    create fields created_at and updated_at

--- src/core/base.dto.ts
+++ src/core/base.dto.ts
@@ -3,4 +3,10 @@ import { ApiProperty } from '@nestjs/swagger';
 export class ListUserDTO {
   @ApiProperty()
   readonly id: string;
+
+  @ApiProperty()
+  readonly created_at: Date;
+
+  @ApiProperty()
+  readonly updated_at: Date;
 }
